e9da97cbb882c6518f7567b18481f25edcf10306
Laura Siebert <laurasiebert80@gmail.com>
2018-10-17 11:16:09 -0500

    added getRepo function

--- main.js
+++ main.js
@@ -1,43 +1,48 @@
 'use strict';
 
 function displayResults(responseJson) {
-  console.log(responseJson);
-  $('#results-list').empty();
-  $('#results').removeClass('hidden');
-  return responseJson.items.forEach(item => {
-    $('#results-list').append(
-        `<li><strong>Username:</strong> ${item.login} <br>
-        <strong>Repository Location:</strong> <a href="http://www.github.com/${item.login}?tab=repositories" target="_blank">http://www.github.com/${item.login}?tab=repositories</a><br>
-        <ul class="sublist">Repository Contents:
-            <li></li>
-        </li>`
-    );
-  })
+    console.log(responseJson);
+    $('#results-list').empty();
+    $('#results').removeClass('hidden');
+    return responseJson.items.forEach(item => {
+        let login = item.login;
+        $('#results-list').append(
+            `<li><strong>Username:</strong> ${login} <br>
+            <strong>Repository Location:</strong> <a href="http://www.github.com/${login}?tab=repositories" target="_blank">http://www.github.com/${login}?tab=repositories</a><br>
+            <ul class="sublist">Repository Contents:
+                <li></li>
+            </li>`
+        );
+        console.log(getRepo(login));
+    })
 };
 
-function getUser(userSearch) {
+function getUsers(userSearch) {
     console.log(`User searched for '${userSearch}'. Function getUser ran.`)
     fetch(`https://api.github.com/search/users?q=${userSearch}`)
       .then(response => response.json())
-      .then(responseJson => 
-        displayResults(responseJson))
-      .catch(error => alert(`Something went wrong. Please try again.`));
+      .then(userResults => 
+        displayResults(userResults))
+      .catch(error => alert(`Something went wrong when getting USERS. Please try again.`));
 }
 
-function getRepo(responseJson) {
-    console.log(`Function getRepo ran.`)
-    fetch(`https://api.github.com/users/${responseJson.items.login}/repos`)
+function getRepo(login) {
+    console.log(`Getting repository list.`);
+    fetch(`https://api.github.com/users/${login}/repos`)
       .then(response => response.json())
-      .then(responseJson1 => 
-        displayResults(responseJson1))
-      .catch(error => alert(`Something went wrong. Please try again.`));
-}
+      .then(userRepos => userRepos.name.forEach(repoName => {
+          let repoName = userRepos.name;
+          $('.sublist').append(`<li>${repoName}</li>`)
+      }
+      ))
+      .catch(error => alert(`Something went wrong when getting REPOSITORIES. Please try again.`));
+}   
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
     const userSearch = $('#js-search-term').val();
-    getUser(userSearch);
+    getUsers(userSearch);
   });
 }
 
